48aae6753c6b6197f04db8c80bff9f50f1e17a97
sohyun <shlim0410@gmail.com>
2023-06-22 22:37:00 +0100

    setup : basic file structures

---
+++ components/Nav.tsx
@@ -0,0 +1,8 @@
+export default function Nav() {
+  return (
+    <>
+      <h1>Home</h1>
+      <h1>About</h1>
+    </>
+  );
+}
---
+++ pages/list/[id].tsx
@@ -0,0 +1,3 @@
+export default function BookDetail() {
+  return <div>I am a book</div>;
+}
